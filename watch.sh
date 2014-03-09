58719bdd2ee674920915963e0bcd97b206a61f6c
Krzysztof Płocharz <krzysztof@plocharz.info>
2014-03-09 12:03:03 +0100

    watch TypeScript files for changes and run tsc

---
+++ watch.sh
@@ -0,0 +1,11 @@
+find static/ts -iname \*.ts | ./bin/entr +notify &
+sleep 1
+
+while read F
+do
+    echo
+    echo -n "Compilling $F ..."
+    tsc $F --sourcemap
+    echo " done"
+done < notify
+
